5e435c4d3ba0d9029eebbbf4f99429f542815a4c
Ruchi <ruchi231202@gmail.com>
2023-09-05 22:43:27 +0530

    Revert "fd"
    This reverts commit 3a2d3db2e9d350360e334bd75a1702cf8e180acb.

--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include_once 'connect.php';
-session_destroy();
-   //echo 'Logged Out!';
-header('Refresh: 1; URL = index.php');
-?>
--- success.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<h1 align=" center"> Successfull!</h1>
-	<div style="margin-left: 400px;">
-	<img src="images/success.png" align="center">
-	</div>
-</html>
-	<?php
-	header('Refresh: 1; URL = index.php');
-?>
